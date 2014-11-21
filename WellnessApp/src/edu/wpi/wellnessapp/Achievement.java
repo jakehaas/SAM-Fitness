1aa3a8bb2e93c985cf0c21d064d103e011514254
Jake Haas <jake@jakehaas.com>
2014-11-21 14:14:30 -0500

    Added stub for achiev database update, added check to only display achiev to user if the database update worked

--- WellnessApp/src/edu/wpi/wellnessapp/Achievement.java
+++ WellnessApp/src/edu/wpi/wellnessapp/Achievement.java
@@ -6,36 +6,50 @@ import android.content.DialogInterface;
 import android.view.View;
 
 public class Achievement {
-    
-   Context ctx = null;
-   int achievementID = -1;
+
+    Context ctx = null;
+    int achievementID = -1;
 
     public Achievement(View v, int achievID) {
 	ctx = v.getContext();
 	achievementID = achievID;
-	displayDialog();
-    }
-    
-    public void displayDialog()
-    {
-	AlertDialog.Builder alert = new AlertDialog.Builder(ctx);
 	
+	// Update the database with a record of this achiev
+	if (updateDatabase()) {
+	    // If the database update was successful, show the alert
+	    displayDialog();
+	} else {
+	    // Could not save record of achiev. Handle properly -- (maybe try again every 10 secs for 1 min, then fail?)
+	}
+	
+    }
+
+    private void displayDialog() {
+	AlertDialog.Builder alert = new AlertDialog.Builder(ctx);
+
 	alert.setTitle("Achievement Unlocked!");
 	alert.setMessage("Congrats!");
-	
+
 	alert.setPositiveButton(R.string.achiv_view,
 		new DialogInterface.OnClickListener() {
 		    public void onClick(DialogInterface dialog, int which) {
-			// continue with delete
+			// Load achiev view
 		    }
 		});
-	
+
 	alert.setNegativeButton(R.string.achiv_close,
 		new DialogInterface.OnClickListener() {
 		    public void onClick(DialogInterface dialog, int which) {
-			// do nothing
+			// Just close the popup...
 		    }
-		}).setIcon(android.R.drawable.ic_dialog_alert).show();
+		});
+
+	alert.setIcon(android.R.drawable.ic_dialog_alert);
+	alert.show();
+    }
+    
+    private boolean updateDatabase() {
+	return true; // TMP FOR NOW UNTIL WE HAVE A REAL DATABASE HANDLER
     }
 
 }
