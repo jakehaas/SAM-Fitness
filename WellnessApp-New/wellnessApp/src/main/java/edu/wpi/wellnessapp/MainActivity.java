81e3aceb51759b53969211e457cde6abe4faa805
Nate Ford <ndford@wpi.edu>
2015-03-18 23:56:35 -0400

    Added graph when less than 2 datapoints

--- WellnessApp-New/wellnessApp/src/main/java/edu/wpi/wellnessapp/MainActivity.java
+++ WellnessApp-New/wellnessApp/src/main/java/edu/wpi/wellnessapp/MainActivity.java
@@ -26,6 +26,7 @@ import android.app.ActionBar.TabListener;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
@@ -99,7 +100,7 @@ public class MainActivity extends FragmentActivity implements TabListener {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_OAUTH) {
-            StepsFragment fragment = (StepsFragment) getSupportFragmentManager()
+            Fragment fragment =  getSupportFragmentManager()
                     .findFragmentById(R.id.pager);
             fragment.onActivityResult(requestCode, resultCode, data);
 
